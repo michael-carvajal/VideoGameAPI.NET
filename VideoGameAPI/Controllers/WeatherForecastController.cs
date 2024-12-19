32932c66a482426dcd03840984981efd10887b50
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-12-18 22:07:52 -0500

    added skibidi to forecast model

--- VideoGameAPI/Controllers/WeatherForecastController.cs
+++ VideoGameAPI/Controllers/WeatherForecastController.cs
@@ -25,7 +25,8 @@ namespace VideoGameAPI.Controllers
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                Skibidi = (index % 2 != 0) ? "Toilet" : "Fornite"  
             })
             .ToArray();
         }
