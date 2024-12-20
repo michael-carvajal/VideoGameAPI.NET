cac759fa21208c6e7bc8c66ae0dcd1632d087623
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-12-19 21:42:51 -0500

    created and seeded data to db

--- VideoGameAPI/Data/VideoGameDbContext.cs
+++ VideoGameAPI/Data/VideoGameDbContext.cs
@@ -4,6 +4,41 @@ namespace VideoGameAPI.Data
 {
     public class VideoGameDbContext(DbContextOptions<VideoGameDbContext> options) : DbContext(options)
     {
-        public  DbSet<VideoGame> VideoGames => Set<VideoGame>();
+        public DbSet<VideoGame> VideoGames => Set<VideoGame>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VideoGame>().HasData(
+                new VideoGame
+                {
+
+                    Id = 1,
+                    Title = "Spider-Man 2",
+                    Platform = "PS5",
+                    Developer = "Insomniac Games",
+                    Publisher = "Sony Interactive Entertainment"
+                },
+                new VideoGame
+                {
+                
+                    Id = 2,
+                    Title = "The Legend of Zelda: Breath of the Wild",
+                    Platform = "Nintendo Switch",
+                    Developer = "Nintendo EPD",
+                    Publisher = "Nintendo"
+                }, 
+                new VideoGame
+                {
+                
+                    Id = 3,
+                    Title = "Sonic the hedgefund",
+                    Platform = "SONY",
+                    Developer = "NVDIA",
+                    Publisher = "SONY Pictures"
+                }
+
+                );
+        }
     }
 }
--- VideoGameAPI/Migrations/20241220024047_Seeding.Designer.cs
+++ VideoGameAPI/Migrations/20241220024047_Seeding.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VideoGameAPI.Data;
 
@@ -10,9 +11,11 @@ using VideoGameAPI.Data;
 namespace VideoGameAPI.Migrations
 {
     [DbContext(typeof(VideoGameDbContext))]
-    partial class VideoGameDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241220024047_Seeding")]
+    partial class Seeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
